b9a8b18098a764cb59f1d2430d09762ad0f8c5a0
Vladimir Lebedev <d3fl4t3@gmail.com>
2022-03-14 18:14:55 +0700

    Multiple commands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::error::Error;
-use std::fs::read_to_string;
+use std::fs::{read_to_string, File};
 use std::path::PathBuf;
 use liteclient::LiteClient;
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use std::io::Read;
+use chrono::{DateTime, Utc};
+use std::time::{Duration, UNIX_EPOCH};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
@@ -12,6 +15,20 @@ struct Args {
     /// LiteClient config
     #[clap(short, long, parse(from_os_str), value_name = "FILE", default_value = "./config.json")]
     config: PathBuf,
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Send external message
+    #[clap(arg_required_else_help = true, parse(from_os_str))]
+    Send {
+        /// File to send
+        file: PathBuf,
+    },
+    /// Get time from liteserver
+    GetTime,
 }
 
 #[tokio::main]
@@ -20,7 +37,18 @@ async fn main() -> Result<()> {
     let args = Args::parse();
     let config = read_to_string(args.config)?;
     let mut client = LiteClient::connect(&config).await?;
-    let result = client.get_time().await?;
-    println!("result = {:?}", result);
+    match &args.command {
+        Commands::GetTime => {
+            let result = client.get_time().await?;
+            println!("Current time: {} => {:?}", result.now(), UNIX_EPOCH + Duration::from_secs(*result.now() as u64));
+        }
+        Commands::Send { file } => {
+            let mut data = Vec::new();
+            File::open(file)?.read_to_end(&mut data)?;
+            let result = client.send_external_message(data).await?;
+            println!("result = {:?}", result);
+        }
+    };
+
     Ok(())
 }
