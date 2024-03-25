f424142b92352bfa65c3f25e1a99f70b8268a438
Vladimir Lebedev <d3fl4t3@gmail.com>
2024-03-25 20:25:27 +0700

    upd tl

--- liteapi/src/tl/request.rs
+++ liteapi/src/tl/request.rs
@@ -40,6 +40,151 @@ pub struct WaitMasterchainSeqno {
 
 #[derive(TlRead, TlWrite, Derivative)]
 #[derivative(Debug, Clone, PartialEq)]
+pub struct GetMasterchainInfoExt {
+    mode: u32
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetBlock {
+    id: BlockIdExt,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetState {
+    id: BlockIdExt,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetBlockHeader {
+    id: BlockIdExt,
+    mode: u32,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct SendMessage {
+    body: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetAccountState {
+    id: BlockIdExt,
+    account: AccountId,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct RunSmcMethod {
+    mode: u32,
+    id: BlockIdExt,
+    account: AccountId,
+    method_id: i64,
+    params: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetShardInfo {
+    id: BlockIdExt,
+    workchain: i32,
+    shard: u64,
+    exact: bool,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetAllShardsInfo {
+    id: BlockIdExt,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetOneTransaction {
+    id: BlockIdExt,
+    account: AccountId,
+    lt: i64,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetTransactions {
+    count: i32,
+    account: AccountId,
+    lt: i64,
+    hash: Int256,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct LookupBlock {
+    #[tl(flags)]
+    mode: (),
+    id: BlockId,
+    #[tl(flags_bit = "mode.1")]
+    lt: Option<i64>,
+    #[tl(flags_bit = "mode.2")]
+    utime: Option<i32>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct ListBlockTransactions {
+    id: BlockIdExt,
+    #[tl(flags)]
+    mode: (),
+    count: i32,
+    #[tl(flags_bit = "mode.7")]
+    after: Option<TransactionId3>,
+    #[tl(flags_bit = "mode.6")]
+    reverse_order: Option<()>,
+    #[tl(flags_bit = "mode.5")]
+    want_proof: Option<()>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetBlockProof {
+    #[tl(flags)]
+    mode: (),
+    known_block: BlockIdExt,
+    #[tl(flags_bit = "mode.0")]
+    target_block: Option<BlockIdExt>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetConfigAll {
+    mode: i32,
+    id: BlockIdExt,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetConfigParams {
+    mode: i32,
+    id: BlockIdExt,
+    param_list: Vec<i32>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct GetValidatorStats {
+    #[tl(flags)]
+    mode: (),
+    id: BlockIdExt,
+    limit: i32,
+    #[tl(flags_bit = "mode.0")]
+    start_after: Option<Int256>,
+    #[tl(flags_bit = "mode.2")]
+    modified_after: Option<i32>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
 #[tl(boxed)]
 pub enum Request {
     /// liteServer.getMasterchainInfo = liteServer.MasterchainInfo;
@@ -48,9 +193,7 @@ pub enum Request {
 
     /// liteServer.getMasterchainInfoExt mode:# = liteServer.MasterchainInfoExt;
     #[tl(id = 0x70a671df)]
-    GetMasterchainInfoExt {
-        mode: u32,
-    },
+    GetMasterchainInfoExt(GetMasterchainInfoExt),
 
     /// liteServer.getTime = liteServer.CurrentTime;
     #[tl(id = 0x16ad5a34)]
@@ -62,142 +205,65 @@ pub enum Request {
 
     /// liteServer.getBlock id:tonNode.blockIdExt = liteServer.BlockData;
     #[tl(id = 0x6377cf0d)]
-    GetBlock {
-        id: BlockIdExt,
-    },
+    GetBlock(GetBlock),
 
     /// liteServer.getState id:tonNode.blockIdExt = liteServer.BlockState;
     #[tl(id = 0xba6e2eb6)]
-    GetState {
-        id: BlockIdExt,
-    },
+    GetState(GetState),
 
     /// liteServer.getBlockHeader id:tonNode.blockIdExt mode:# = liteServer.BlockHeader;
     #[tl(id = 0x21ec069e)]
-    GetBlockHeader {
-        id: BlockIdExt,
-        mode: u32,
-    },
+    GetBlockHeader(GetBlockHeader),
 
     /// liteServer.sendMessage body:bytes = liteServer.SendMsgStatus;
     #[tl(id = 0x690ad482)]
-    SendMessage {
-        body: Vec<u8>,
-    },
+    SendMessage(SendMessage),
 
     /// liteServer.getAccountState id:tonNode.blockIdExt account:liteServer.accountId = liteServer.AccountState;
     #[tl(id = 0x6b890e25)]
-    GetAccountState {
-        id: BlockIdExt,
-        account: AccountId,
-    },
+    GetAccountState(GetAccountState),
 
     /// liteServer.runSmcMethod mode:# id:tonNode.blockIdExt account:liteServer.accountId method_id:long params:bytes = liteServer.RunMethodResult;
     #[tl(id = 0x5cc65dd2)]
-    RunSmcMethod {
-        mode: u32,
-        id: BlockIdExt,
-        account: AccountId,
-        method_id: i64,
-        params: Vec<u8>,
-    },
+    RunSmcMethod(RunSmcMethod),
 
     /// liteServer.getShardInfo id:tonNode.blockIdExt workchain:int shard:long exact:Bool = liteServer.ShardInfo;
     #[tl(id = 0x46a2f425)]
-    GetShardInfo {
-        id: BlockIdExt,
-        workchain: i32,
-        shard: u64,
-        exact: bool,
-    },
+    GetShardInfo(GetShardInfo),
 
     /// liteServer.getAllShardsInfo id:tonNode.blockIdExt = liteServer.AllShardsInfo;
     #[tl(id = 0x74d3fd6b)]
-    GetAllShardsInfo {
-        id: BlockIdExt,
-    },
+    GetAllShardsInfo(GetAllShardsInfo),
 
     /// liteServer.getOneTransaction id:tonNode.blockIdExt account:liteServer.accountId lt:long = liteServer.TransactionInfo;
     #[tl(id = 0xd40f24ea)]
-    GetOneTransaction {
-        id: BlockIdExt,
-        account: AccountId,
-        lt: i64,
-    },
+    GetOneTransaction(GetOneTransaction),
 
     /// liteServer.getTransactions count:# account:liteServer.accountId lt:long hash:int256 = liteServer.TransactionList;
     #[tl(id = 0x1c40e7a1)]
-    GetTransactions {
-        count: i32,
-        account: AccountId,
-        lt: i64,
-        hash: Int256,
-    },
+    GetTransactions(GetTransactions),
 
     /// liteServer.lookupBlock mode:# id:tonNode.blockId lt:mode.1?long utime:mode.2?int = liteServer.BlockHeader;
     #[tl(id = 0xfac8f71e)]
-    LookupBlock {
-        #[tl(flags)]
-        mode: (),
-        id: BlockId,
-        //#[tl(skip, flags_bit = "mode.0")]
-        //trash: Option<u8>,
-        #[tl(flags_bit = "mode.1")]
-        lt: Option<i64>,
-        #[tl(flags_bit = "mode.2")]
-        utime: Option<i32>,
-    },
+    LookupBlock(LookupBlock),
 
     /// liteServer.listBlockTransactions id:tonNode.blockIdExt mode:# count:# after:mode.7?liteServer.transactionId3 reverse_order:mode.6?true want_proof:mode.5?true = liteServer.BlockTransactions;
     #[tl(id = 0xadfcc7da)]
-    ListBlockTransactions {
-        id: BlockIdExt,
-        #[tl(flags)]
-        mode: (),
-        count: i32,
-        #[tl(flags_bit = "mode.7")]
-        after: Option<TransactionId3>,
-        #[tl(flags_bit = "mode.6")]
-        reverse_order: Option<()>,
-        #[tl(flags_bit = "mode.5")]
-        want_proof: Option<()>,
-    },
+    ListBlockTransactions(ListBlockTransactions),
 
     /// liteServer.getBlockProof mode:# known_block:tonNode.blockIdExt target_block:mode.0?tonNode.blockIdExt = liteServer.PartialBlockProof;
     #[tl(id = 0x8aea9c44)]
-    GetBlockProof {
-        #[tl(flags)]
-        mode: (),
-        known_block: BlockIdExt,
-        #[tl(flags_bit = "mode.0")]
-        target_block: Option<BlockIdExt>,
-    },
+    GetBlockProof(GetBlockProof),
 
     /// liteServer.getConfigAll mode:# id:tonNode.blockIdExt = liteServer.ConfigInfo;
     #[tl(id = 0x911b26b7)]
-    GetConfigAll {
-        mode: i32,
-        id: BlockIdExt,
-    },
+    GetConfigAll(GetConfigAll),
 
     /// liteServer.getConfigParams mode:# id:tonNode.blockIdExt param_list:(vector int) = liteServer.ConfigInfo;
     #[tl(id = 0x2a111c19)]
-    GetConfigParams {
-        mode: i32,
-        id: BlockIdExt,
-        param_list: Vec<i32>,
-    },
+    GetConfigParams(GetConfigParams),
 
     /// liteServer.getValidatorStats#091a58bc mode:# id:tonNode.blockIdExt limit:int start_after:mode.0?int256 modified_after:mode.2?int = liteServer.ValidatorStats;
     #[tl(id = 0xe7253699)]
-    GetValidatorStats {
-        #[tl(flags)]
-        mode: (),
-        id: BlockIdExt,
-        limit: i32,
-        #[tl(flags_bit = "mode.0")]
-        start_after: Option<Int256>,
-        #[tl(flags_bit = "mode.2")]
-        modified_after: Option<i32>,
-    },
+    GetValidatorStats(GetValidatorStats),
 }
--- liteapi/src/tl/response.rs
+++ liteapi/src/tl/response.rs
@@ -6,201 +6,281 @@ use super::utils::*;
 
 #[derive(TlRead, TlWrite, Derivative)]
 #[derivative(Debug, Clone, PartialEq)]
+pub struct MasterchainInfo {
+    last: BlockIdExt,
+    state_root_hash: Int256,
+    init: ZeroStateIdExt,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct MasterchainInfoExt {
+    #[tl(flags)]
+    mode: (),
+    version: i32,
+    capabilities: i64,
+    last: BlockIdExt,
+    last_utime: i32,
+    now: i32,
+    state_root_hash: Int256,
+    init: ZeroStateIdExt,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct CurrentTime {
+    now: i32,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct Version {
+    mode: i32,
+    version: i32,
+    capabilities: i64,
+    now: i32,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct BlockData {
+    id: BlockIdExt,
+    data: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct BlockState {
+    id: BlockIdExt,
+    root_hash: Int256,
+    file_hash: Int256,
+    data: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct BlockHeader {
+    id: BlockIdExt,
+    mode: i32,
+    header_proof: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct SendMsgStatus {
+    status: i32,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct AccountState {
+    id: BlockIdExt,
+    shardblk: BlockIdExt,
+    #[derivative(Debug(format_with = "fmt_bytes"))]
+    shard_proof: Vec<u8>,
+    #[derivative(Debug(format_with = "fmt_bytes"))]
+    proof: Vec<u8>,
+    #[derivative(Debug(format_with = "fmt_bytes"))]
+    state: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct RunMethodResult {
+    #[tl(flags)]
+    mode: (),
+    id: BlockIdExt,
+    shardblk: BlockIdExt,
+    #[tl(flags_bit = "mode.0")]
+    shard_proof: Option<Vec<u8>>,
+    #[tl(flags_bit = "mode.0")]
+    proof: Option<Vec<u8>>,
+    #[tl(flags_bit = "mode.1")]
+    state_proof: Option<Vec<u8>>,
+    #[tl(flags_bit = "mode.3")]
+    init_c7: Option<Vec<u8>>,
+    #[tl(flags_bit = "mode.4")]
+    lib_extras: Option<Vec<u8>>,
+    exit_code: i32,
+    #[tl(flags_bit = "mode.2")]
+    result: Option<Vec<u8>>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct ShardInfo {
+    id: BlockIdExt,
+    shardblk: BlockIdExt,
+    shard_proof: Vec<u8>,
+    shard_descr: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct AllShardsInfo {
+    id: BlockIdExt,
+    proof: Vec<u8>,
+    data: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct TransactionInfo {
+    id: BlockIdExt,
+    proof: Vec<u8>,
+    transaction: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct TransactionList {
+    ids: Vec<BlockIdExt>,
+    transactions: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct TransactionId {
+    #[tl(flags)]
+    mode: (),
+    #[tl(flags_bit = "mode.0")]
+    account: Option<Int256>,
+    #[tl(flags_bit = "mode.1")]
+    lt: Option<i64>,
+    #[tl(flags_bit = "mode.2")]
+    hash: Option<Int256>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct BlockTransactions {
+    id: BlockIdExt,
+    req_count: i32,
+    incomplete: bool,
+    ids: Vec<TransactionId>,
+    proof: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct PartialBlockProof {
+    complete: bool,
+    from: BlockIdExt,
+    to: BlockIdExt,
+    steps: Vec<BlockLink>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct ConfigInfo {
+    #[tl(flags)]
+    mode: (),
+    id: BlockIdExt,
+    state_proof: Vec<u8>,
+    config_proof: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct ValidatorStats {
+    #[tl(flags)]
+    mode: (),
+    id: BlockIdExt,
+    count: i32,
+    complete: bool,
+    state_proof: Vec<u8>,
+    data_proof: Vec<u8>,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
+pub struct Error {
+    code: i32,
+    #[derivative(Debug(format_with = "String::fmt"))]
+    message: String,
+}
+
+#[derive(TlRead, TlWrite, Derivative)]
+#[derivative(Debug, Clone, PartialEq)]
 #[tl(boxed)]
 pub enum Response {
     /// liteServer.masterchainInfo last:tonNode.blockIdExt state_root_hash:int256 init:tonNode.zeroStateIdExt = liteServer.MasterchainInfo;
     #[tl(id = 0x85832881)]
-    MasterchainInfo {
-        last: BlockIdExt,
-        state_root_hash: Int256,
-        init: ZeroStateIdExt,
-    },
+    MasterchainInfo(MasterchainInfo),
 
     /// liteServer.masterchainInfoExt mode:# version:int capabilities:long last:tonNode.blockIdExt last_utime:int now:int state_root_hash:int256 init:tonNode.zeroStateIdExt = liteServer.MasterchainInfoExt;
     #[tl(id = 0xa8cce0f5)]
-    MasterchainInfoExt {
-        #[tl(flags)]
-        mode: (),
-        version: i32,
-        capabilities: i64,
-        last: BlockIdExt,
-        last_utime: i32,
-        now: i32,
-        state_root_hash: Int256,
-        init: ZeroStateIdExt,
-    },
+    MasterchainInfoExt(MasterchainInfoExt),
 
     /// liteServer.currentTime now:int = liteServer.CurrentTime;
     #[tl(id = 0xe953000d)]
-    CurrentTime {
-        now: i32,
-    },
+    CurrentTime(CurrentTime),
 
     /// liteServer.version mode:# version:int capabilities:long now:int = liteServer.Version;
     #[tl(id = 0x5a0491e5)]
-    Version {
-        mode: i32,
-        version: i32,
-        capabilities: i64,
-        now: i32,
-    },
+    Version(Version),
 
     /// liteServer.blockData id:tonNode.blockIdExt data:bytes = liteServer.BlockData;
     #[tl(id = 0xa574ed6c)]
-    BlockData {
-        id: BlockIdExt,
-        data: Vec<u8>,
-    },
+    BlockData(BlockData),
 
     /// liteServer.blockState id:tonNode.blockIdExt root_hash:int256 file_hash:int256 data:bytes = liteServer.BlockState;
     #[tl(id = 0xabaddc0c)]
-    BlockState {
-        id: BlockIdExt,
-        root_hash: Int256,
-        file_hash: Int256,
-        data: Vec<u8>,
-    },
+    BlockState(BlockState),
 
     /// liteServer.blockHeader id:tonNode.blockIdExt mode:# header_proof:bytes = liteServer.BlockHeader;
     #[tl(id = 0x752d8219)]
-    BlockHeader {
-        id: BlockIdExt,
-        mode: i32,
-        header_proof: Vec<u8>,
-    },
+    BlockHeader(BlockHeader),
 
     /// liteServer.sendMsgStatus status:int = liteServer.SendMsgStatus;
     #[tl(id = 0x3950e597)]
-    SendMsgStatus {
-        status: i32,
-    },
+    SendMsgStatus(SendMsgStatus),
 
     /// liteServer.accountState id:tonNode.blockIdExt shardblk:tonNode.blockIdExt shard_proof:bytes proof:bytes state:bytes = liteServer.AccountState;
     #[tl(id = 0x7079c751)]
-    AccountState {
-        id: BlockIdExt,
-        shardblk: BlockIdExt,
-        #[derivative(Debug(format_with = "fmt_bytes"))]
-        shard_proof: Vec<u8>,
-        #[derivative(Debug(format_with = "fmt_bytes"))]
-        proof: Vec<u8>,
-        #[derivative(Debug(format_with = "fmt_bytes"))]
-        state: Vec<u8>,
-    },
+    AccountState(AccountState),
 
     /// liteServer.runMethodResult mode:# id:tonNode.blockIdExt shardblk:tonNode.blockIdExt shard_proof:mode.0?bytes proof:mode.0?bytes state_proof:mode.1?bytes init_c7:mode.3?bytes lib_extras:mode.4?bytes exit_code:int result:mode.2?bytes = liteServer.RunMethodResult;
     #[tl(id = 0xa39a616b)]
-    RunMethodResult {
-        #[tl(flags)]
-        mode: (),
-        id: BlockIdExt,
-        shardblk: BlockIdExt,
-        #[tl(flags_bit = "mode.0")]
-        shard_proof: Option<Vec<u8>>,
-        #[tl(flags_bit = "mode.0")]
-        proof: Option<Vec<u8>>,
-        #[tl(flags_bit = "mode.1")]
-        state_proof: Option<Vec<u8>>,
-        #[tl(flags_bit = "mode.3")]
-        init_c7: Option<Vec<u8>>,
-        #[tl(flags_bit = "mode.4")]
-        lib_extras: Option<Vec<u8>>,
-        exit_code: i32,
-        #[tl(flags_bit = "mode.2")]
-        result: Option<Vec<u8>>,
-    },
+    RunMethodResult(RunMethodResult),
 
     /// liteServer.shardInfo id:tonNode.blockIdExt shardblk:tonNode.blockIdExt shard_proof:bytes shard_descr:bytes = liteServer.ShardInfo;
     #[tl(id = 0x9fe6cd84)]
-    ShardInfo {
-        id: BlockIdExt,
-        shardblk: BlockIdExt,
-        shard_proof: Vec<u8>,
-        shard_descr: Vec<u8>,
-    },
+    ShardInfo(ShardInfo),
 
     /// liteServer.allShardsInfo id:tonNode.blockIdExt proof:bytes data:bytes = liteServer.AllShardsInfo;
     #[tl(id = 0x098fe72d)]
-    AllShardsInfo {
-        id: BlockIdExt,
-        proof: Vec<u8>,
-        data: Vec<u8>,
-    },
+    AllShardsInfo(AllShardsInfo),
 
     /// liteServer.transactionInfo id:tonNode.blockIdExt proof:bytes transaction:bytes = liteServer.TransactionInfo;
     #[tl(id = 0x0edeed47)]
-    TransactionInfo {
-        id: BlockIdExt,
-        proof: Vec<u8>,
-        transaction: Vec<u8>,
-    },
+    TransactionInfo(TransactionInfo),
 
     /// liteServer.transactionList ids:(vector tonNode.blockIdExt) transactions:bytes = liteServer.TransactionList;
     #[tl(id = 0x6f26c60b)]
-    TransactionList {
-        ids: Vec<BlockIdExt>,
-        transactions: Vec<u8>,
-    },
+    TransactionList(TransactionList),
 
     /// liteServer.transactionId mode:# account:mode.0?int256 lt:mode.1?long hash:mode.2?int256 = liteServer.TransactionId;
     #[tl(id = 0xb12f65af)]
-    TransactionId {
-        #[tl(flags)]
-        mode: (),
-        #[tl(flags_bit = "mode.0")]
-        account: Option<Int256>,
-        #[tl(flags_bit = "mode.1")]
-        lt: Option<i64>,
-        #[tl(flags_bit = "mode.2")]
-        hash: Option<Int256>,
-    },
+    TransactionId(TransactionId),
 
     /// liteServer.blockTransactions id:tonNode.blockIdExt req_count:# incomplete:Bool ids:(vector liteServer.transactionId) proof:bytes = liteServer.BlockTransactions;
     #[tl(id = 0xbd8cad2b)]
-    BlockTransactions {
-        id: BlockIdExt,
-        req_count: i32,
-        incomplete: bool,
-        ids: Vec<TransactionId>,
-        proof: Vec<u8>,
-    },
+    BlockTransactions(BlockTransactions),
 
     /// liteServer.partialBlockProof complete:Bool from:tonNode.blockIdExt to:tonNode.blockIdExt steps:(vector liteServer.BlockLink) = liteServer.PartialBlockProof;
     #[tl(id = 0x8ed0d2c1)]
-    PartialBlockProof {
-        complete: bool,
-        from: BlockIdExt,
-        to: BlockIdExt,
-        steps: Vec<BlockLink>,
-    },
+    PartialBlockProof(PartialBlockProof),
 
     /// liteServer.configInfo mode:# id:tonNode.blockIdExt state_proof:bytes config_proof:bytes = liteServer.ConfigInfo;
     #[tl(id = 0xae7b272f)]
-    ConfigInfo {
-        #[tl(flags)]
-        mode: (),
-        id: BlockIdExt,
-        state_proof: Vec<u8>,
-        config_proof: Vec<u8>,
-    },
+    ConfigInfo(ConfigInfo),
 
     /// liteServer.validatorStats mode:# id:tonNode.blockIdExt count:int complete:Bool state_proof:bytes data_proof:bytes = liteServer.ValidatorStats;
     #[tl(id = 0xb9f796d8)]
-    ValidatorStats {
-        #[tl(flags)]
-        mode: (),
-        id: BlockIdExt,
-        count: i32,
-        complete: bool,
-        state_proof: Vec<u8>,
-        data_proof: Vec<u8>,
-    },
+    ValidatorStats(ValidatorStats),
 
     /// liteServer.error code:int message:string = liteServer.Error;
     #[tl(id = 0xbba9e148)]
-    Error {
-        code: i32,
-        #[derivative(Debug(format_with = "String::fmt"))]
-        message: String,
-    }
+    Error(Error),
 }
